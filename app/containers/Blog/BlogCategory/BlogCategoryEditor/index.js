41fbf91e3415a3f30ab4fd154b130fec83a4fdeb
Om Chhetri <chhetriom01@gmail.com>
2019-05-29 20:31:23 +0545

    created the form for the blog category

--- app/containers/Blog/BlogCategory/BlogCategoryEditor/index.js
+++ app/containers/Blog/BlogCategory/BlogCategoryEditor/index.js
@@ -18,20 +18,83 @@ import makeSelectBlogCategoryEditor from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
+import { Link } from 'react-router-dom';
+import { Form, Button, Checkbox } from 'semantic-ui-react';
 
 /* eslint-disable react/prefer-stateless-function */
 export class BlogCategoryEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        categoryName: 'FoosBall',
+        categoryDescription: 'Indoor Game, popular in BitsBeat Office',
+        active: false,
+      },
+    };
+  }
+
+  onInputChange = e => {
+    const field = e.target.name;
+    const data = this.state.data;
+    data[field] = e.target.value;
+    return this.setState({
+      data: data,
+    });
+  };
+
+  handleCheck = e => {
+    this.setState(prevState => ({
+      ...prevState,
+      data: {
+        ...prevState.data,
+        active: !prevState.data.active,
+      },
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
     return (
       <div>
-        <Helmet>
-          <title>BlogCategoryEditor</title>
-          <meta
-            name="description"
-            content="Description of BlogCategoryEditor"
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Input
+            label="Blog Category"
+            placeholder="category name"
+            name="categoryName"
+            value={this.state.data.categoryName}
+            onChange={this.onInputChange}
+            required
           />
-        </Helmet>
-        <FormattedMessage {...messages.header} />
+
+          <Form.Input
+            label="Blog Title"
+            placeholder="Enter personName"
+            name="categoryDescription"
+            value={this.state.data.categoryDescription}
+            onChange={this.onInputChange}
+            required
+          />
+          <Form.Input>
+            <Checkbox
+              checked={this.state.data.active}
+              onChange={this.handleCheck}
+              label="Active"
+              toggle
+            />
+          </Form.Input>
+
+          <Button color="blue" type="Submit">
+            Submit
+          </Button>
+          <Link to="/admin/blog/blogcategory">
+            <Button color="red" onClick={this.resetvalue}>
+              Cancel
+            </Button>
+          </Link>
+        </Form>
       </div>
     );
   }
